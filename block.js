90000e8ecd9a8b01a22c7730e1a3b9aa26eea001
Bibi10 <bdjafalo@outlook.com>
2019-06-05 23:46:54 +0200

    Add Block Class

---
+++ block.js
@@ -0,0 +1,21 @@
+class Block {
+  constructor( timestamp, lastHash, Hash, data) {
+    this.timestamp  = timestamp;  //Bind timestamp value and bind it to timesamp property
+    this.lastHash = lastHash;
+    this.Hash = Hash;
+    this.data = data;
+  }
+
+  toString(){
+    return `Block - 
+      Timestamp : ${this.timestamp}
+      Last Hash : ${this.lastHash.substring(0,10)}
+      Hash      : ${this.Hash.substring(0,10)}
+      Data      : ${this.data};
+
+    `
+  }
+
+}
+
+module.exports  = Block;
