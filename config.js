67cd48889406c3da372dae637fb5d2484cc1f25f
GYPI1 <40456175+GYPI1@users.noreply.github.com>
2019-06-23 23:19:02 +0200

    save config.js block.tes.js index.test.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const DIFFICULTY = 4
 const MINE_RATE = 3000
 const INITIAL_BALANCE = 3000
 const MINING_REWARD = 55
+const FUT_COIN = FC
 
-module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE, MINING_REWARD }
+module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE, MINING_REWARD, FUT_COIN}
 
 //Contain our POW algorithm Config
